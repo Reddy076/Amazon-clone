12d41f6c8975abcfa38d124c969d15cf67d3eea9
Dasthagiri <uranusfor@gmail.com>
2025-10-01 09:43:03 +0530

    delete products from html when clicking delete

--- scripts/Checkout.js
+++ scripts/Checkout.js
@@ -17,7 +17,8 @@ products.forEach((product)=>{
 });
 cartSummaryHTML+=
   `
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -107,5 +108,8 @@ cartSummaryHTML+=
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       clearCart(productId);
+
+      const container=document.querySelector(`.js-cart-item-container-${productId}`);
+      container.remove();
     });
   });
